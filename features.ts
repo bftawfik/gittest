afbca8f6fac19ff1badc4b4c0e0ee2d4092a08df
Bishoy Khalil <bftawfik@gmail.com>
2024-05-13 10:26:23 +0300

    Revert "Revert "add new features""
    This reverts commit e1e80e80d1150f0a6880972acd0e24bd1e2033d1.

--- features.ts
+++ features.ts
@@ -2,6 +2,9 @@ const features = {
   feature1: true,
   feature2: false,
   feature3: true,
+  feature4: false,
+  feature5: true,
+  feature6: false,
 };
 export const getFeatures = (featureName: string) => {
   return !!features[featureName];
